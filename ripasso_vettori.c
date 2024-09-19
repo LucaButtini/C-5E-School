15b47d4bae5eef6a9656e602eb767b790af401e5
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-09-19 08:44:00 +0200

    Update ripasso_vettori.c

--- ripasso_vettori.c
+++ ripasso_vettori.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#define DIM 50
 
 void menu ()
 {
@@ -20,5 +21,31 @@ void menu ()
 int main()
 {
     int scelta;
+    int numeri[DIM];
+    switch(scelta)
+    {
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 5:
+        break;
+        case 6:
+        break;
+        case 7:
+        break;
+        case 8:
+        break;
+        case 9:
+        break;
+        default:
+        break;
+    }
     return 0;
 }
