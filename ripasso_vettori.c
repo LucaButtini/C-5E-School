ca34e1d2a4549281fb876952cfced64a2afb9caa
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-09-19 08:59:33 +0200

    Update ripasso_vettori.c

--- ripasso_vettori.c
+++ ripasso_vettori.c
@@ -1,8 +1,11 @@
-#include <stdio.h
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <bool.h>
 #define DIM 50
+
+
 
 void menu ()
 {
@@ -15,37 +18,46 @@ void menu ()
     printf("7 -> Elimina\n");
     printf("8 -> Alterni\n");
     printf("9 -> Ordinamento\n");
+    printf("10 -> Esci\n");
     
 }
 
 int main()
 {
     int scelta;
+    bool esci = false;
     int numeri[DIM];
-    switch(scelta)
-    {
-        case 1:
-        break;
-        case 2:
-        break;
-        case 3:
-        break;
-        case 4:
-        break;
-        case 5:
-        break;
-        case 5:
-        break;
-        case 6:
-        break;
-        case 7:
-        break;
-        case 8:
-        break;
-        case 9:
-        break;
-        default:
-        break;
-    }
+    
+    do{
+        menu();printf("Inserisci la scelta:\n");
+        scanf("%d", &scelta);
+        switch(scelta)
+            {
+            case 1:
+            printf("dsds\n");    
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+            case 6:
+            break;
+            case 7:
+            break;
+            case 8:
+            break;
+            case 9:
+            break;
+            case 10:
+            esci = 1;
+            default:
+            printf("Scela errata\n");
+            break;
+            }
+    }while(esci==0);
     return 0;
 }
