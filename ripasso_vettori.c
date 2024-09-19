bac49b73c6af781543522a7de4c09a5ebda46a4a
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-09-19 08:38:43 +0200

    Update ripasso_vettori.c

--- ripasso_vettori.c
+++ ripasso_vettori.c
@@ -1,8 +1,24 @@
-#include <stdio.h>
+#include <stdio.h
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void menu ()
+{
+    printf("1 -> Stampa\n");
+    printf("2 -> Stampa Invertito\n");
+    printf("3 -> Stampa somma e media\n");
+    printf("4-> Stampa Pari\n");
+    printf("5 -> Stampa Dipari\n");
+    printf("6 -> Ricerca\n");
+    printf("7 -> Elimina\n");
+    printf("8 -> Alterni\n");
+    printf("9 -> Ordinamento\n");
+    
+}
 
 int main()
 {
-    printf("Hello World");
-
+    int scelta;
     return 0;
 }
