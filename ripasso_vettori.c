bc883f9b887c96047d5a977a873410961e1e1077
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2024-09-22 17:13:43 +0200

    punti 1 e 2

--- ripasso_vettori.c
+++ ripasso_vettori.c
@@ -1,11 +1,21 @@
-#include <bool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
+void stampa(int numeri[], int dim) {
+  for (int i = 0; i < dim; i++) {
+    printf("[%d] --> %d\n", i, numeri[i]);
+  }
+}
+void stampa_contrario(int numeri[], int dim) {
+  for (int i = dim - 1; i > 0; i--) {
+    printf("[%d] --> %d\n", i, numeri[i]);
+  }
+}
+
 void menu() {
-  printf("1 -> Stampa\n");
+  printf("\n1 -> Stampa\n");
   printf("2 -> Stampa Invertito\n");
   printf("3 -> Stampa somma e media\n");
   printf("4-> Stampa Pari\n");
@@ -14,49 +24,65 @@ void menu() {
   printf("7 -> Elimina\n");
   printf("8 -> Alterni\n");
   printf("9 -> Ordinamento\n");
-  printf("10 -> Esci\n");
+  printf("10 -> Esci\n\n");
 }
 
 int main() {
   int scelta, dim = 0;
-  bool esci = false;
+  int esci = 0;
   srand(time(NULL));
   printf("Inserisci la dimensione dell'array\n");
   scanf("%d", &dim);
   int numeri[dim];
-  for (int i = 0; i < size; i++) {
-    numeri[i] = rand() % 100; // Random numbers between 0 and 99
+  for (int i = 0; i < dim; i++) {
+    numeri[i] = rand() % 100;
   }
+
   do {
     menu();
     printf("Inserisci la scelta:\n");
     scanf("%d", &scelta);
     switch (scelta) {
     case 1:
-      printf("dsds\n");
+      stampa(numeri, dim);
       break;
     case 2:
+      stampa_contrario(numeri, dim);
       break;
     case 3:
+
       break;
     case 4:
+
       break;
     case 5:
+
       break;
-    case 6:
+    case 6: {
+      int val;
+      printf("Inserisci il numero da cercare: ");
+      scanf("%d", &val);
       break;
-    case 7:
+    }
+    case 7: {
+      int val;
+      printf("Inserisci il numero da eliminare: ");
+      scanf("%d", &val);
       break;
+    }
     case 8:
+
       break;
     case 9:
+
       break;
     case 10:
       esci = 1;
+      break;
     default:
-      printf("Scela errata\n");
+      printf("Scelta errata.\n");
       break;
     }
-  } while (esci == 0);
+  } while (!esci);
   return 0;
 }
