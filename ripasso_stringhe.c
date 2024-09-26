615c52824f798a3fde0948d4c3430cc6767020d0
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-09-26 08:55:12 +0200

    da finire punto 2

--- ripasso_stringhe.c
+++ ripasso_stringhe.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #define DIM 1024
 
-int contiene_lettere(char stringa[]){
+int contiene_lettere(char stringa[]){ //punto 1
     int check = 1;
     for(int i=0; i<strlen(stringa); i++){
        if(!(stringa[i]>= 'A' && stringa[i] <= 'Z' || stringa[i]>= 'a' && stringa[i] <= 'z')){
@@ -14,9 +14,23 @@ int contiene_lettere(char stringa[]){
     return check;
 }
 
+int conta_lettere(char stringa[], char car){ //punto 2
+    int conta=0;
+     for(int i=0; i<strlen(stringa); i++){
+         if(stringa[i]== car){
+             conta++;
+         }else{
+             conta=0;
+         }
+     }
+     return conta;
+}
+
 int main()
 {
     char stringa1[DIM];
+    char car;
+    int conta;
     printf("Inserisci una stringa a piacere: ");
     scanf("%s", stringa1);
     if(contiene_lettere(stringa1) == 1 ){
@@ -24,5 +38,10 @@ int main()
     }else{
         printf("La stringa non contiene solo lettere\n");
     }
+    
+    printf("Inserisci la lettera che vuoi ricercare: ");
+    scanf("%c", car);
+    conta = conta_lettere(stringa1, car);
+    printf("La lettera")
     return 0;
 }
