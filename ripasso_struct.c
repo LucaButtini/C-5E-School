56372279940c7fb4c0428d7dccec1bac667afb54
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-10-01 08:23:52 +0200

    Create ripasso_struct.c

---
+++ ripasso_struct.c
@@ -0,0 +1,18 @@
+#include <stdio.h
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char modello_auto[100];
+    char marca[100];
+    int cilindrata;
+    double prezzo;
+    int anno_immatricolazione;
+}Auto;
+
+int main(int argc, char *argv[])
+{
+    int dim=0;
+    
+    return 0;
+}
