9b7a089a6d824fb118192f54a997897ccdc60fe6
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-10-01 08:42:35 +0200

    Update ripasso_struct.c

--- ripasso_struct.c
+++ ripasso_struct.c
@@ -1,18 +1,28 @@
 #include <stdio.h
 #include <stdlib.h>
 #include <string.h>
+#define DIM 3
 
-typedef struct{
-    char modello_auto[100];
-    char marca[100];
-    int cilindrata;
-    double prezzo;
-    int anno_immatricolazione;
-}Auto;
+typedef struct {
+	char modello_auto[100];
+	char marca[100];
+	int cilindrata;
+	double prezzo;
+	int anno_immatricolazione;
+} Auto;
+
+void stampa(Auto concessionaria[]){
+    for(int i=0; i<DIM; i++){
+        printf("Modello: [%s], Marca: [%s]")
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    int dim=0;
-    
-    return 0;
+	char modello[100], marca[100];
+	Auto a1 = {"modello 1", "marca 1", 80, 600.54, 2009 };
+	Auto a2 = {"modello 2", "marca 2", 50, 300.34, 2013 };
+	Auto a3 = {"modello 3", "marca 3", 100, 800.53, 1996};
+	Auto concessionaria[DIM] = {a1, a2, a3};
+	return 0;
 }
