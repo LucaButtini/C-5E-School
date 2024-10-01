eb8b12b85d94fb199aa8ec2a1219ca6e37dfb621
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-10-01 09:22:35 +0200

    Update ripasso_struct.c

--- ripasso_struct.c
+++ ripasso_struct.c
@@ -1,4 +1,4 @@
-#include <stdio.h
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define DIM 3
@@ -11,18 +11,40 @@ typedef struct {
 	int anno_immatricolazione;
 } Auto;
 
-void stampa(Auto concessionaria[]){
-    for(int i=0; i<DIM; i++){
-        printf("Modello: [%s], Marca: [%s]")
-    }
+int ricerca_prezzo(Auto conc[], double ricerca) {
+	for(int i=0; i<DIM; i++) {
+		if(ricerca== conc[i].prezzo) {
+			return i;
+			break;
+		}
+	}
+	return -1;
+}
+
+void stampa(Auto conc[]) {
+	for(int i=0; i<DIM; i++) {
+		printf("Modello: [%s], Marca: [%s], Cilindrata: [%d], Prezzo: [%.2f], Anno Immatricolazione: [%d]\n", conc[i].modello_auto, conc[i].marca, conc[i].cilindrata, conc[i].prezzo, conc[i].anno_immatricolazione);
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	char modello[100], marca[100];
+	int posizione;
+	double ricercato;
 	Auto a1 = {"modello 1", "marca 1", 80, 600.54, 2009 };
 	Auto a2 = {"modello 2", "marca 2", 50, 300.34, 2013 };
 	Auto a3 = {"modello 3", "marca 3", 100, 800.53, 1996};
 	Auto concessionaria[DIM] = {a1, a2, a3};
+	stampa(concessionaria);
+	printf("\nInserisci il prezzo della macchina da ricercare\n");
+	scanf("%.2f", &ricercato);
+	posizione =  ricerca_prezzo(concessionaria, ricercato);
+	if(posizione != -1){
+	    printf("\nPrezzo [%.2f], Modello [%s]\n", ricercato, concessionaria[posizione].modello_auto);
+	}
+	else{
+	    printf("\nMacchina non presente nella concessionaria\n");
+	}
 	return 0;
 }
