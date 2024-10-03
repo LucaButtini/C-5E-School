55e8835b7de9b132c0d8dd1803c76912d3695d90
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2024-10-03 10:12:45 +0200

    Add files via upload

--- Libreria_digitale/libreria.c
+++ Libreria_digitale/libreria.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define DIM 38 // dimensione libreria
+#define BUFFER_DIM 1024
 
 typedef struct {
     char title[TITLE_SIZE];
@@ -32,14 +33,37 @@ typedef enum {
 
 Libro libreria[DIM];
 
-void leggi_file()
-{
+//https://www.geeksforgeeks.org/relational-database-from-csv-files-in-c/
+void leggi_file(){
+    char buffer[BUFFER_DIM];
+    int colonna=0, riga=0;
+    FILE *file = fopen("libreria_libri.csv", "r");
+    if(file == NULL){
+        printf("Errore apertura file\n");
+        exit(1); 
+    }
+while(fgets(buffer, BUFFER_DIM,file)){
+    colonna=0;
+    riga++;
+    if(riga==1){
+        char *value = strok(buffer, ",");
+        while(value){
+            if(colonna==0)
+            {
+                
+            }
+        }
+    }
+}
 
 }
 
 int main(int argc, char *argv[])
 {
-    
+    if(argc!= 2){
+        printf("Errore numero argomenti\n");
+        exit(1);
+    }
 
     return 0;
 }
