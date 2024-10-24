21bc65775405418d25850653820ab5e9a826cf15
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2024-10-24 16:27:46 +0200

    fnziona cliet e server

--- Sockets/Esercizio2/client.c
+++ Sockets/Esercizio2/client.c
@@ -1,33 +1,34 @@
+// client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <errno.h>
-#include <ctype.h>
 #include <unistd.h>
 
-#define DIM_STR 100
+#define DIM_STR 1024
 #define SERVERPORT 1313
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Errore argomenti\n");
+        printf("Errore: fornire una stringa come argomento.\n");
         exit(-1);
     }
-    char stringa[DIM_STR];
+    char risposta[DIM_STR];
     struct sockaddr_in servizio;
     int socketfd;
+
     servizio.sin_family = AF_INET;
     servizio.sin_port = htons(SERVERPORT);
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
 
     socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    connect(socketfd, (struct sockaddr_in *)&servizio, sizeof(servizio));
-    write(socketfd, argv[1], sizeof(argv[1]));
+    connect(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+    write(socketfd, argv[1], strlen(argv[1]) + 1);
+    read(socketfd, risposta, sizeof(risposta));
+    printf("%s", risposta);
     close(socketfd);
     return 0;
 }
--- Sockets/Esercizio2/server.c
+++ Sockets/Esercizio2/server.c
@@ -1,9 +1,63 @@
+// server.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <errno.h>
 #include <ctype.h>
 #include <unistd.h>
+
+#define DIM_STR 1024
+#define SERVERPORT 1313
+
+void conta_cons_e_voc(char stringa[], int *cons, int *voc)
+{
+    char lettera;
+    *voc = 0;
+    *cons = 0;
+
+    for (int i = 0; i < strlen(stringa); i++)
+    {
+        lettera = tolower(stringa[i]);
+        if (isalpha(lettera))
+        {
+            if (lettera == 'a' || lettera == 'e' || lettera == 'i' || lettera == 'o' || lettera == 'u')
+                (*voc)++;
+            else
+                (*cons)++;
+        }
+    }
+}
+
+int main()
+{
+    struct sockaddr_in servizio;
+    int socketfd, soa, fromlen = sizeof(servizio), cons, voc;
+    char stringa[DIM_STR], risposta[DIM_STR];
+
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVERPORT);
+
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+    listen(socketfd, 10);
+
+    for (;;)
+    {
+        printf("\n\nServer in ascolto...");
+        fflush(stdout);
+        soa = accept(socketfd, (struct sockaddr *)&servizio, &fromlen);
+        read(soa, stringa, sizeof(stringa));
+        conta_cons_e_voc(stringa, &cons, &voc);
+        int len = snprintf(risposta, sizeof(risposta), "Stringa -> [%s], Vocali -> [%d], Consonanti -> [%d]\n", stringa, voc, cons);
+        if (len >= sizeof(risposta))
+        {
+            fprintf(stderr, "Attenzione: l'output è stato troncato!\n");
+        }
+        write(soa, risposta, strlen(risposta) + 1);
+        close(soa);
+    }
+    close(socketfd);
+    return 0;
+}
