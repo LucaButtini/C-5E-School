da278b0d53cd285f06451bcc7d6d6216b7bc71fb
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2024-10-30 22:22:01 +0100

    es 4 va

--- Sockets/Esercizio4/client.c
+++ Sockets/Esercizio4/client.c
@@ -2,38 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <errno.h>
+#include <ctype.h>
 #include <unistd.h>
 
-#define DIM_STR 1024
+#define DIM 50
 #define SERVERPORT 1313
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_in servizio;
-    int socketfd, occorrenze = 0;
-    char stringa[DIM_STR], car;
-
-    printf("Inserisci una stringa: ");
-    scanf("%s", stringa);
-
-    printf("Inserisci un carattere da contare: ");
-    scanf(" %c", &car);
+    char str[DIM];
+    char car;
+    int socketfd, occorrenze;
 
     servizio.sin_family = AF_INET;
-    servizio.sin_port = htons(SERVERPORT);
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVERPORT);
 
     socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  
     connect(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
 
-    write(socketfd, stringa, strlen(stringa) + 1);
-    write(socketfd, &car, 1);
+    
+    printf("Inserisci una stringa:\n");
+    scanf("%s", str);
+    printf("Inserisci un carattere da ricercare:\n");
+    scanf(" %c", &car);
 
+
+    write(socketfd, str, sizeof(str));
+    write(socketfd, &car, sizeof(char));
+
+    
     read(socketfd, &occorrenze, sizeof(int));
     printf("Occorrenze del carattere '%c': %d\n", car, occorrenze);
 
+    
     close(socketfd);
     return 0;
 }
--- Sockets/Esercizio4/server.c
+++ Sockets/Esercizio4/server.c
@@ -2,29 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <errno.h>
+#include <ctype.h>
 #include <unistd.h>
 
-#define DIM_STR 1024
+#define DIM 50
 #define SERVERPORT 1313
 
-int conta_occorrenze(char stringa[], char car)
+// Funzione per contare le occorrenze del carattere 'car' nella stringa 'str'
+int conta_occorrenze(char car, char str[])
 {
     int conta = 0;
-    for (int i = 0; i < strlen(stringa); i++)
+    for (int i = 0; i < strlen(str); i++)
     {
-        if (car == stringa[i])
+        if (str[i] == car)
             conta++;
     }
-    return conta; 
+    return conta;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    struct sockaddr_in servizio;
-    int socketfd, soa, fromlen = sizeof(servizio), occorrenze;
-    char car, stringa[DIM_STR];
+    struct sockaddr_in servizio, addr_remoto;
+    int socketfd, soa, fromlen = sizeof(addr_remoto);
+    char str[DIM];
+    char car;
+    int occorrenze;
 
     servizio.sin_family = AF_INET;
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -38,18 +44,22 @@ int main()
     {
         printf("\n\nServer in ascolto...");
         fflush(stdout);
+        soa = accept(socketfd, (struct sockaddr *)&addr_remoto, &fromlen);
 
-        soa = accept(socketfd, (struct sockaddr *)&servizio, &fromlen);
+            read(soa, str, sizeof(str));
+        read(soa, &car, sizeof(char));
 
-        read(soa, stringa, sizeof(stringa));
-        read(soa, &car, sizeof(car));
+        printf("Stringa ricevuta: %s\n", str);
+        printf("Carattere ricevuto: %c\n", car);
 
-        occorrenze = conta_occorrenze(stringa, car);
+        occorrenze = conta_occorrenze(car, str);
+        printf("Il carattere è presente %d volte\n", occorrenze);
 
         write(soa, &occorrenze, sizeof(int));
 
         close(soa);
     }
 
+    close(socketfd);
     return 0;
 }
