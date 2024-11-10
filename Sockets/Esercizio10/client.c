411eb67b60dbfa7ab149c01e52672fdd575148d7
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2024-11-10 11:51:36 +0100

    es 10

--- Sockets/Esercizio10/client.c
+++ Sockets/Esercizio10/client.c
@@ -1,52 +1,52 @@
-// client.c
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <ctype.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#include <limits.h>
 
-#define DIM_STR 1024
+#define DIM 120
 #define SERVERPORT 1313
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        printf("Errore argomenti\n");
-        exit(-1);
-    }
-
     struct sockaddr_in servizio;
-    int socketfd, confronto = 0;
-
     servizio.sin_family = AF_INET;
-    servizio.sin_port = htons(SERVERPORT);
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVERPORT);
+
+    char str1[DIM], str2[DIM];
+    char messaggio[DIM];
+    int socketfd;
 
     socketfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    connect(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
-
-    write(socketfd, argv[1], strlen(argv[1]) + 1);
-    write(socketfd, argv[2], strlen(argv[2]) + 1);
-
-    read(socketfd, &confronto, sizeof(confronto));
-
-    if (confronto == -1)
+    if (socketfd < 0)
     {
-        printf("\nLe stringhe hanno la stessa lunghezza\n");
+        perror("Errore nella creazione del socket");
+        exit(EXIT_FAILURE);
     }
-    else if (confronto == 0)
+
+    if (connect(socketfd, (struct sockaddr *)&servizio, sizeof(servizio)) < 0)
     {
-        printf("\nLa stringa 2 è la più lunga\n");
+        perror("Errore nella connessione al server");
+        close(socketfd);
+        exit(EXIT_FAILURE);
     }
-    else
-    {
-        printf("\nLa stringa 1 è la più lunga\n");
-    }
+
+    printf("Inserisci la prima stringa: ");
+    fgets(str1, DIM, stdin);
+    str1[strcspn(str1, "\n")] = 0;
+
+    printf("Inserisci la seconda stringa: ");
+    fgets(str2, DIM, stdin);
+    str2[strcspn(str2, "\n")] = 0;
+
+    write(socketfd, str1, sizeof(str1));
+    write(socketfd, str2, sizeof(str2));
+
+    read(socketfd, messaggio, sizeof(messaggio));
+    printf("Risultato dal server: %s\n", messaggio);
 
     close(socketfd);
     return 0;
--- Sockets/Esercizio10/server.c
+++ Sockets/Esercizio10/server.c
@@ -1,40 +1,57 @@
-// server.c
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <ctype.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#include <limits.h>
 
-#define DIM_STR 1024
+#define DIM 120
 #define SERVERPORT 1313
 
-int confronta_stringhe(char str1[], char str2[])
+void confronta_lunghezza(const char *str1, const char *str2, char *messaggio)
 {
-    if (strlen(str1) == strlen(str2))
-        return -1;
-    else if (strlen(str1) < strlen(str2))
-        return 0;
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+
+    if (len1 > len2)
+    {
+        strcpy(messaggio, "La prima stringa è più lunga della seconda.");
+    }
+    else if (len1 < len2)
+    {
+        strcpy(messaggio, "La seconda stringa è più lunga della prima.");
+    }
     else
-        return 1;
+    {
+        strcpy(messaggio, "Le due stringhe hanno la stessa lunghezza.");
+    }
 }
 
-int main(int argc, char *argv[])
+int main()
 {
     struct sockaddr_in servizio, addr_remoto;
-    int socketfd, soa, fromlen = sizeof(addr_remoto);
-    char str1[DIM_STR], str2[DIM_STR];
-    int confronto;
-
+    servizio.sin_family = AF_INET;
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
     servizio.sin_port = htons(SERVERPORT);
-    servizio.sin_family = AF_INET;
+
+    int socketfd, soa, fromlen = sizeof(addr_remoto);
+    char str1[DIM], str2[DIM];
+    char messaggio[DIM];
 
     socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd < 0)
+    {
+        perror("Errore nella creazione del socket");
+        exit(EXIT_FAILURE);
+    }
 
-    bind(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+    if (bind(socketfd, (struct sockaddr *)&servizio, sizeof(servizio)) < 0)
+    {
+        perror("Errore nel binding");
+        close(socketfd);
+        exit(EXIT_FAILURE);
+    }
 
     listen(socketfd, 10);
 
@@ -43,17 +60,18 @@ int main(int argc, char *argv[])
         printf("Server in ascolto...\n");
         fflush(stdout);
 
-        soa = accept(socketfd, (struct sockaddr *)&addr_remoto, &fromlen);
+        soa = accept(socketfd, (struct sockaddr *)&addr_remoto, (socklen_t *)&fromlen);
 
         read(soa, str1, sizeof(str1));
         read(soa, str2, sizeof(str2));
 
-        confronto = confronta_stringhe(str1, str2);
-        
-        write(soa, &confronto, sizeof(confronto));
+        confronta_lunghezza(str1, str2, messaggio);
+
+        write(soa, messaggio, sizeof(messaggio));
 
         close(soa);
     }
+
     close(socketfd);
     return 0;
 }
